ca42d6f09119751f401933ddcfabebbfeab17a89
Noah Martin-Ruben <nmartinr@umich.edu>
2022-06-12 12:07:53 -0400

    Add a bit more details to consumer group idea comment

--- main.go
+++ main.go
@@ -13,9 +13,10 @@ func main() {
 	// channel to consume from
 	// What should probably happen is that the chat receiver creates a channel in a map keyed off of
 	// a key given in the register call (or doesn't if it already exists, this key acts as a sort of
-	// consumer group), then returns a handle object that holds the key and provides access to the
-	// channel. Client then must defer handle.Close() to close out the handle, which closes the
-	// channel and removes it from the map.
+	// consumer group) (consumer group can also be chosen by a url parameter by the connecting client),
+	// then returns a handle object that holds the key and provides access to the channel. Client
+	// then must defer handle.Close() to close out the handle, which closes the channel and removes
+	// it from the map.
 	messageBufferSize := 100
 	messages := make(chan string, messageBufferSize)
 
